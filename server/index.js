f62a5eb7a8cbfc5d2db989475d943428f5fd7f01
izumshahid <irzumshahid@gmail.com>
2026-02-07 18:40:41 +0500

    inital commit

--- server/index.js
+++ server/index.js
@@ -12,17 +12,19 @@ app.use(cors());
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 5001;
+const MongoURL = "mongodb://localhost:27017/west_shoe";
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(MongoURL);
     console.log(`App is connected to the database.`);
   } catch (error) {
     console.error(`Error connecting to DB: ${error.message}`);
     process.exit(1);
   }
 };
+
+app.use("/api", router);
 
 const startServer = async () => {
   try {
